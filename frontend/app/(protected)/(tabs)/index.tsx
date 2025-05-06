aefe009214fd60ccbae11df356bce0be78f823f8
aduong3 <mythalink@gmail.com>
2025-05-05 17:24:06 -0700

    Added dummy data for showing chores, need to separate by person

--- frontend/app/(protected)/(tabs)/index.tsx
+++ frontend/app/(protected)/(tabs)/index.tsx
@@ -1,20 +1,44 @@
 import { useContext } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, FlatList, Text, View } from "react-native";
 import { AuthContext } from "../../../utils/authContext";
+
+const fakeData = [
+  {
+    id: 1,
+    title: "Clean Kitchen",
+    person: "Andrew",
+  },
+  {
+    id: 2,
+    title: "Take out trash",
+    person: "Bri",
+  },
+];
 
 export default function Index() {
   const authState = useContext(AuthContext);
   const { logOut } = authState;
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Press the button to log out!</Text>
-      <Button title="Log Out" onPress={logOut} />
-    </View>
+    <>
+      <View className="flex-1 items-center justify-center">
+        <Text>Log Out Here</Text>
+        <Button onPress={logOut} title="Log Out" />
+      </View>
+      <View>
+        <Text className="text-3xl font-semibold">Chores</Text>
+        <FlatList
+          data={fakeData}
+          contentContainerStyle={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          renderItem={({ item }) => (
+            <Text>
+              {item.title} - {item.person}
+            </Text>
+          )}
+        ></FlatList>
+      </View>
+    </>
   );
 }
