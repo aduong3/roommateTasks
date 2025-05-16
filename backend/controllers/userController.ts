73c9d8c7089e83fe95fcfb0cee35189f83f9c67e
aduong3 <mythalink@gmail.com>
2025-05-16 14:14:48 -0700

    Need to fix jwt types in userController

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,12 +1,26 @@
 import admin from "firebase-admin";
 import { Request, Response } from "express";
 import serviceAccount from "../firebase/firebase-service-account.json";
+import jwt from "jsonwebtoken";
+import { SignOptions } from "jsonwebtoken";
 
 import User from "../models/userModel";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
+
+const jwt_secret: string = process.env.EXPO_PUBLIC_JWT_SECRET!;
+const jwt_expires_in: string = process.env.EXPO_PUBLIC_JWT_EXPIRES_IN!;
+const options: SignOptions = {
+  expiresIn: jwt_expires_in,
+};
+
+const signToken = (id: string): string => {
+  jwt.sign({ id }, jwt_secret, {
+    expiresIn: jwt_expires_in,
+  });
+};
 
 export const verifyGoogleLogIn = async (req: Request, res: Response) => {
   try {
@@ -18,25 +32,18 @@ export const verifyGoogleLogIn = async (req: Request, res: Response) => {
 
     // console.log("decodedToken:", decodedToken);
 
-    const user = await User.findOne({ email });
+    let user = await User.findOne({ email });
 
-    if (user) {
-      // console.log(user);
-      res.status(200).json({
-        status: "success",
-        data: {
-          user,
-        },
-      });
-    } else {
-      const newUser = await User.create({ email, photo: picture, name });
-      res.status(200).json({
-        status: "success",
-        data: {
-          user: newUser,
-        },
-      });
+    if (!user) {
+      user = await User.create({ email, photo: picture, name });
     }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
   } catch (err) {
     if (err instanceof Error) {
       res.status(400).json({
