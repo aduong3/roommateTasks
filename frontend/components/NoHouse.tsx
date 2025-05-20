896a707bdad9e5802403a59e835c2be1e86d342f
aduong3 <mythalink@gmail.com>
2025-05-20 11:21:31 -0700

    Added react-hook-form for joining or creating a group. Added components folder to Nativewind config.
    Modal is made and able to scale size. Need to add in form for inputting the group code.

--- frontend/components/NoHouse.tsx
+++ frontend/components/NoHouse.tsx
@@ -4,32 +4,43 @@ import React, { useState } from "react";
 const NoHouse = () => {
   const [inputCode, setInputCode] = useState(false);
   return (
-    <View className="flex-1 justify-center items-center mt-8 px-3">
-      <Text className="text-xl font-bold text-center">
-        You are currently not in a household! Create or join one to start
-        creating tasks with your roommates!
-      </Text>
-      <View className="flex-row gap-10 mt-5">
-        <TouchableOpacity
-          className="bg-blue-500 px-4 py-2 rounded-lg"
-          onPress={() => setInputCode(!inputCode)}
-        >
-          <Text className="text-white font-semibold text-xl">
-            Join with Code
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="bg-blue-500 px-4 py-2 rounded-lg">
-          <Text className="text-white font-semibold text-xl">
-            Create Household
-          </Text>
-        </TouchableOpacity>
+    <>
+      <View>
+        <Modal visible={inputCode} transparent={true}>
+          <View className="flex-1 bg-transparent justify-center items-center">
+            <View className="w-[90%] h-[25%] bg-blue-200">
+              <TouchableOpacity
+                onPress={() => setInputCode(false)}
+                className="bg-blue-500"
+              >
+                <Text>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
       </View>
-      {inputCode && (
-        <View className="mt-16">
-          <Text>Input code here</Text>
+      <View className="flex-1 justify-center items-center mt-8 px-3">
+        <Text className="text-xl font-bold text-center">
+          You are currently not in a household! Create or join one to start
+          creating tasks with your roommates!
+        </Text>
+        <View className="flex-row gap-10 mt-5">
+          <TouchableOpacity
+            className="bg-blue-500 px-4 py-2 rounded-lg"
+            onPress={() => setInputCode(!inputCode)}
+          >
+            <Text className="text-white font-semibold text-xl">
+              Join with Code
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-blue-500 px-4 py-2">
+            <Text className="text-white font-semibold text-xl">
+              Create Household
+            </Text>
+          </TouchableOpacity>
         </View>
-      )}
-    </View>
+      </View>
+    </>
   );
 };
 
