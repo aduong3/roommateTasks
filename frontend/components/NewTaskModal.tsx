b82cbd28ff289338dafa1ca33f6d3a5f668884e8
aduong3 <mythalink@gmail.com>
2025-05-27 18:26:33 -0700

    Added first Controller form for modal.
    Need to look for npm packages that mimic React's <select>

--- frontend/components/NewTaskModal.tsx
+++ frontend/components/NewTaskModal.tsx
@@ -1,23 +1,67 @@
-import { View, Text, Modal, TouchableOpacity } from "react-native";
+import { View, Text, Modal, TouchableOpacity, TextInput } from "react-native";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 
 type NewTaskModalProps = {
   visible: boolean;
   setVisible: (value: boolean) => void;
 };
 
+type NewTaskDataInput = {
+  name: string;
+  dueDate: Date;
+  recurrence: string;
+  assignedTo: string;
+};
+
 const NewTaskModal = ({ visible, setVisible }: NewTaskModalProps) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<NewTaskDataInput>({
+    defaultValues: {
+      name: "",
+      dueDate: new Date(),
+      recurrence: "",
+      assignedTo: "",
+    },
+  });
+
   return (
     <View>
       <Modal visible={visible} transparent={true}>
         <View className="flex-1 bg-transparent justify-center items-center">
-          <View className="w-[90%] h-[30%] bg-blue-300 rounded-lg">
-            <TouchableOpacity
-              className="bg-gray-200"
-              onPress={() => setVisible(false)}
-            >
-              <Text>Close Modal</Text>
-            </TouchableOpacity>
+          <View className="w-full h-[50%] bg-blue-300 rounded-lg">
+            <View className="flex-1 items-center py-6 px-4">
+              <Controller
+                control={control}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    placeholder="Task name"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    className="py-2 px-3 text-xl"
+                  />
+                )}
+                name="name"
+              />
+              {/* Date picker */}
+              {/* select picker for recurrence */}
+              {/* assignedTo picker. Get all users and choose by name, but saved as user's id. */}
+            </View>
+            <View className="flex justify-center items-center py-2">
+              <TouchableOpacity
+                className="bg-blue-500 py-2 px-3 rounded-lg"
+                onPress={() => setVisible(false)}
+              >
+                <Text className="text-white font-semibold text-lg">
+                  Close Modal
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Modal>
