f2acd6abb63b2cb2d999e57acde573950c5c2e22
aduong3 <mythalink@gmail.com>
2025-06-08 14:03:49 -0700

    feat: Add basic outline to how I want the ping system to be. I want to replace Alert with toasts.
    The Alert function is stands-out too much and is just so sudden. Toasts would be way better and less "up in your face".

--- frontend/app/(protected)/(tabs)/teamMembers.tsx
+++ frontend/app/(protected)/(tabs)/teamMembers.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Alert, TouchableOpacity } from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import { getHouseholdMembers } from "../../../services/apiHouse";
 import { AuthContext } from "../../../utils/authContext";
@@ -29,10 +29,31 @@ const TeamMembers = () => {
     };
     fetchMembers();
   }, [user?.houseId]);
+
+  const pingMember = async (name: string, userId: string) => {
+    try {
+      Alert.alert(`You just pinged ${name}!`);
+    } catch (error) {
+      Alert.alert("Failed to send ping!");
+    }
+  };
+
   return (
     <View className="flex justify-center items-center py-20">
+      <Text className="text-3xl">Ping a Member!</Text>
       {membersList.map((member) => (
-        <Text key={member?.userId}>{member?.name}</Text>
+        <View
+          key={member?.userId}
+          className="flex-row justify-around items-center w-full px-3 py-6"
+        >
+          <Text className="text-2xl">{member?.name}</Text>
+          <TouchableOpacity
+            className="bg-blue-400 px-3 py-2 rounded-lg"
+            onPress={() => pingMember(member?.name, member?.userId)}
+          >
+            <Text className="text-2xl text-white">Ping</Text>
+          </TouchableOpacity>
+        </View>
       ))}
     </View>
   );
